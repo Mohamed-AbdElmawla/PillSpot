0455f9c88d2a88c588ab258cd44b00ec8a899baf
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-02-21 03:06:15 +0200

    Sort added to the Result table

--- Front/pill-spot/src/components/Result/ResultTable/index.tsx
+++ Front/pill-spot/src/components/Result/ResultTable/index.tsx
@@ -1,33 +1,77 @@
-
-
+import { useState } from "react";
 import { Pharmcs } from "../data";
 import TableRow from "../TableRow/TableRow";
+import { IoPricetagsOutline } from "react-icons/io5";
+import { FaSortAlphaDown } from "react-icons/fa";
+import { FaSortAlphaUpAlt } from "react-icons/fa";
+import { LuMap } from "react-icons/lu";
+
+
+
 
 const ResultTable = () => {
+  const [resPhar, setResPhar] = useState(Pharmcs);
+  const [priceIsAscending, setpriceIsAscending] = useState(true);
+  const [distanceIsAscending, setdistanceIsAscending] = useState(true);
 
+  function sortDistance() {
+    setResPhar((prev) =>
+      [...prev].sort((a, b) =>
+        distanceIsAscending
+          ? Number(a.distance) - Number(b.distance)
+          : Number(b.distance) - Number(a.distance)
+      )
+    );
+    setdistanceIsAscending((prev) => !prev);
+  }
 
+  function sortPrice() {
+    setResPhar((prev) =>
+      [...prev].sort((a, b) =>
+        priceIsAscending
+          ? Number(a.price) - Number(b.price)
+          : Number(b.price) - Number(a.price)
+      )
+    );
+    setpriceIsAscending((prev) => !prev);
+  }
 
-    const Pharmacies = Pharmcs.map((p)=>(
-        <TableRow name={p.name} price={p.price} distance={p.distance} rating={p.rating} imgSrc={p.imgSrc} />
-      )) ; 
-      
+  const Pharmacies = resPhar.map((p) => (
+    <TableRow
+      name={p.name}
+      price={p.price}
+      distance={p.distance}
+      rating={p.rating}
+      imgSrc={p.imgSrc}
+    />
+  ));
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        {/* head */}
-        <thead className="text-[#02457a]" >
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Location</th>
-            <th>Amount</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {Pharmacies}
-        </tbody>
-      </table>
+    <div className="overflow-x-auto mt-100">
+      <div className="flex justify-end gap-2">
+        <button className="btn " onClick={sortPrice}>
+        <IoPricetagsOutline />
+        {priceIsAscending?<FaSortAlphaDown />:<FaSortAlphaUpAlt />}
+        </button>
+        <button className="btn" onClick={sortDistance}>
+        <LuMap />
+        {distanceIsAscending?<FaSortAlphaDown />:<FaSortAlphaUpAlt />}
+        </button>
+      </div>
+      <div>
+        <table className="table">
+          <thead className="text-[#02457a]">
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Location</th>
+              <th>Amount</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{Pharmacies}</tbody>
+        </table>
+      </div>
     </div>
   );
 };
