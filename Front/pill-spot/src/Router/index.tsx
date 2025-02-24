ccce44fc4dc517fb18d61cee7e1b9aa469501134
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-02-24 21:59:01 +0200

    add pharmacy management page and its nested router

--- Front/pill-spot/src/Router/index.tsx
+++ Front/pill-spot/src/Router/index.tsx
@@ -1,26 +1,36 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import Home from "../pages/Landing";
 import RootPage from "../layouts/RootPage";
 import ResultPage from "../pages/ResultPage";
+import PharManagementLayout from "../pages/PharmacyManagement";
+import InventoryPage from "../pages/PharmacyManagement/Inventory";
 
 // Define the router
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootPage />}>
-         <Route path="landing" element={<Home />} />
-         <Route path="result" element={<ResultPage />} />
-
-
+      <Route path="landing" element={<Home />} />
+      <Route path="result" element={<ResultPage />} />
+      <Route path="pharmacymanagement" element={<PharManagementLayout />}>
+        <Route path="pharmanhome" element={"Home"} />
+        <Route path="pharmaninventory" element={<InventoryPage/>} />
+        <Route path="pharmanstaff" element={"pharmacy staff"} />
+        <Route path="pharmananalytics" element={"pharmacy analytics"} />
+        <Route path="pharmanorders" element={"pharmacy orders"} />
+      </Route>
     </Route>
   )
 );
 
 // Main Router Component
 const Router = () => {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
