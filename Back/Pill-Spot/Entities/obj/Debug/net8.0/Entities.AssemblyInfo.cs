6d8cf8d268210e74c9f5b1cf99edffaec8a29d34
uukh22 <khaled.ibrahim033@compit.aun.edu.eg>
2025-06-25 16:50:41 +0300

    Add ImageProfileUrl to PharmacyEmployeesService

--- Back/Pill-Spot/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Back/Pill-Spot/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0352fa6320ecce667a37303c382961789ad81ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae0d9862cab5de853e6ca41488e49e2f1714934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
