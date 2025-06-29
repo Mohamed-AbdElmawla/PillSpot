b0caa505efd7d8da34776f6ff1f89ba810f7e1df
Mohamed-AbdElmawla <mabdelmawla23@gmail.com>
2025-06-29 18:13:07 +0300

    Add: the additional data to the pharmacyProductDto

--- Back/Pill-Spot/Shared/DataTransferObjects/PharmacyProductDto.cs
+++ Back/Pill-Spot/Shared/DataTransferObjects/PharmacyProductDto.cs
@@ -5,5 +5,8 @@
         public int Quantity { get; init; }
         public ProductDto ProductDto { get; init; }
         public PharmacyDto PharmacyDto { get; init; }
+        public bool IsAvailable { get; init; }
+        public int MinimumStockThreshold { get; init; }
+        public DateTime? LastRestocked { get; init; }
     }
 }
